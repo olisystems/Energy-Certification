01ef958e07dbc9ecac2de77492ea9b865567391e
m-yahya <yahya_gis@hotmail.com>
2018-03-12 18:23:54 +0100

    responsive map

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([48.77538056, 9.16277778], 14);
-mapLink =
-  '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-L.tileLayer(
-  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; ' + mapLink + ' Contributors',
-    maxZoom: 18,
-  }).addTo(map);
-L.marker([48.77538056, 9.16277778]).addTo(map);
+  var map = L.map('map').setView([48.77538056, 9.16277778], 14);
+  mapLink =
+    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+  L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; ' + mapLink + ' Contributors',
+      maxZoom: 18,
+    }).addTo(map);
+  L.marker([48.77538056, 9.16277778]).addTo(map);

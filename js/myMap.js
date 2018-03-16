0806036e7bc00651e41d34a3e30013bf50e830b1
Muhammad Yahya <yahya_gis@hotmail.com>
2018-03-17 00:03:00 +0100

    Leaflet map added

--- js/myMap.js
+++ js/myMap.js
@@ -6,4 +6,24 @@
       attribution: '&copy; ' + mapLink + ' Contributors',
       maxZoom: 18,
     }).addTo(map);
-  L.marker([48.77538056, 9.16277778]).addTo(map);
+  var marker = L.marker([48.77538056, 9.16277778]).addTo(map);
+
+// binding popoup
+  marker.bindPopup("OLI Systems GmbH");
+  marker.on('mouseover', function(e) {
+    this.openPopup();
+  });
+  marker.on('mouseout', function(e) {
+    this.closePopup();
+  });
+
+// showing mouse click position
+  var popup = L.popup();
+  function onMapClick(e) {
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+  }
+
+  map.on('click', onMapClick);

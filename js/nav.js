3b33bdc07de749385968c7b20b6157bea426a921
m-yahya <yahya_gis@hotmail.com>
2018-03-19 09:47:21 +0100

    contents updated

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ if ($(event.target).parents(".navbar-collapse").length < 1) {
     var clickover = $(event.target);
     var $navbar = $(".navbar-collapse");
     var _opened = $navbar.hasClass("in");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+    if (_opened === true && !clickover.hasClass("navbar-collapse")) {
         $navbar.collapse('hide');
     }
   }

4a5e2df415d9a26172f87f0d10cbe6415568665f
m-yahya <yahya_gis@hotmail.com>
2019-02-27 12:18:03 +0100

    conf: server file updated

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ console.log('\n local server is running on port ' + port + '!');
 // set up app
 
 // public folder for static files
+
 app.use(express.static('app'));
 
 // enable body parsing for json format
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false

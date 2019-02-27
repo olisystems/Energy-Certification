046c52a100bff562bbeba0fe1a8cf514134538b0
m-yahya <giulianobaumann@googlemail.com>
2019-02-27 12:01:13 +0100

    conf: browser detection implemented

--- src/js/scripts/index.js
+++ src/js/scripts/index.js
@@ -13,6 +13,15 @@ import {
   consRealTimeEner
 } from './consumption.js';
 
+// browser detection
+import Bowser from 'bowser';
+const browser = Bowser.getParser(window.navigator.userAgent);
+//console.log(`The current browser is "${browser.getBrowserName()}"`);
+const getName = browser.getBrowserName();
+if (getName != "Chrome") {
+  alert("Hi there, please open the link in Google Chrome for the best experience!");
+}
+
 // windows onload functions
 async function start() {
   getAllProducers();
